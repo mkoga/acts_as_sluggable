84520398ec861c7083357b23f1a1050733a96afc
Micah Koga <mkoga@oogalabs.com>
2009-04-18 11:38:55 -0700

    * Added to_param option, defaults to true

--- lib/acts_as_slug.rb
+++ lib/acts_as_slug.rb
@@ -6,28 +6,37 @@ module ActsAsSlug
 
   module ActMethods
     
-    def acts_as_slug(opts)
+    def acts_as_slug(column, opts = {})
       class_inheritable_accessor :slug_options
-      self.slug_options = opts.is_a?(Hash) ? opts : { opts => true }
-      self.slug_options[:source] ||= self.slug_options.keys.first || :name
-      self.slug_options[:destination] ||= :slug
+      self.slug_options = opts
+      self.slug_options[:source] ||= column.to_sym || :name
+      self.slug_options[:slug] ||= :slug
+      self.slug_options[:to_param] = opts[:to_param].nil? ? true : opts[:to_param]
 
       class_eval <<-EOV
         before_save :create_slug
       EOV
 
+      if self.slug_options[:to_param]
+        class_eval <<-EOV
+          def to_param
+            self.send(self.slug_options[:slug].to_s)
+          end
+        EOV
+      end
+
       include InstanceMethods
     end
 
     module InstanceMethods
-      
+
       private
 
       def create_slug
         tmp_source = self.send(self.slug_options[:source].to_s)
         tmp_slug = tmp_source.downcase.gsub(/\s+/, '-').gsub(/&/, 'and')
 
-        self.send("#{self.slug_options[:destination].to_s}=", tmp_slug)
+        self.send("#{self.slug_options[:slug].to_s}=", tmp_slug)
       end
 
     end
